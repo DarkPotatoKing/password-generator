1325304ceb17a88ce5ebd2609b4c829cac40eb08
Kyle Rosales <kyle.rosales97@gmail.com>
2015-06-18 13:16:02 +0800

    added command line interface class

---
+++ CLI.py
@@ -0,0 +1,7 @@
+class CLI():
+    """The command line interface class for the password generator."""
+    
+    @classmethod
+    def start(cls):
+        print 'Password Generator'
+        print '------------------'
